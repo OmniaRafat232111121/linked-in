18fc249a5df50a55878a6f8ad5b53aa3d860f058
omnia rafat <76200523+OmniaRafat232111121@users.noreply.github.com>
2023-03-26 00:59:26 +0200

    linkedIn

--- src/redux/reducers/rootReducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import userReducer from "./userReducer";
-import articlesReducer from "./articlesReducer";
-const rootReducer = combineReducers({
-  user: userReducer,
-  articles:articlesReducer
-});
-
-export default rootReducer;
